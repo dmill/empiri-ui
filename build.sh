f7c80c7441decebe0d2d047ee2dc3cba39bffef2
Andrew Wong <arwong09@gmail.com>
2015-08-08 18:28:38 -0700

    implement scss

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-browserify -t [babelify] app/js/application.jsx -o bundle.js
+browserify -t [babelify] ./app/js/application.jsx -o ./build/bundle.js
+node-sass --output-style compressed ./app/stylesheets/application.scss > ./build/application.css
 http-server
