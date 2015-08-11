ce5ae50ea6ec21f08c970cd9aec6184eb77af3a0
Andrew Wong <arwong09@gmail.com>
2015-08-11 03:03:12 -0700

    successfully using requirejs and bower, with configured npm script

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 babel --watch --modules amd app/js --out-dir build/js &
-node-sass --watch --output-style compressed ./app/stylesheets/application.scss > ./build/application.css &
-node-sass --output-style compressed ./app/stylesheets/application.scss > ./build/application.css
+node-sass --watch --output-style compressed ./app/stylesheets/application.scss ./build/application.css &
 http-server
--- build/js/dispatchers/publication_dispatcher.js
+++
@@ -1,7 +0,0 @@
-define(["exports", "module", "flux"], function (exports, module, _flux) {
-  "use strict";
-
-  var PublicationDispatcher = new _flux.Dispatcher();
-
-  module.exports = PublicationDispatcher;
-});
