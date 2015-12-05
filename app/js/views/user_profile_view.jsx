cd24d08c18b53d1eda26dde0170421e9933489a9
Andrew Wong <arwong09@gmail.com>
2015-12-04 21:31:22 -0800

    change heading size on name

--- app/js/views/user_profile_view.jsx
+++ app/js/views/user_profile_view.jsx
@@ -16,7 +16,7 @@ export default class UserProfileView extends Component {
       return (
         <div id="user-profile-view" className="sidebar four columns">
           <img src={profile.picture} />
-          <h2>{profile.name}</h2>
+          <h5>{profile.name}</h5>
           <h6>{profile.headline}</h6>
           <p>{profile.summary}</p>
         </div>
