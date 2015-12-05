832dfddde7aee341db7db7542a26b9817c94b274
Andrew Wong <arwong09@gmail.com>
2015-12-05 11:22:51 -0800

    rename variables

--- app/js/views/browse_view.jsx
+++ app/js/views/browse_view.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import store from '../redux/store'
 
-const Pub = (props) => {
+const PublicationPreview = (props) => {
   return (
     <div style={{border: "1px solid #bbb", borderRadius: "3px"}}>
       <h4>{props.title}</h4>
@@ -23,16 +23,16 @@ export default class BrowseView extends Component {
         <div className="row">
           <div className="seven columns">
             <h1>Trending Publications</h1>
-            <Pub title={this.props.data.title} picture={user.picture} name={user.name} />
-            <Pub title={this.props.data.title} picture={user.picture} name={user.name} />
-            <Pub title={this.props.data.title} picture={user.picture} name={user.name} />
-            <Pub title={this.props.data.title} picture={user.picture} name={user.name} />
-            <Pub title={this.props.data.title} picture={user.picture} name={user.name} />
+            <PublicationPreview title={this.props.data.title} picture={user.picture} name={user.name} />
+            <PublicationPreview title={this.props.data.title} picture={user.picture} name={user.name} />
+            <PublicationPreview title={this.props.data.title} picture={user.picture} name={user.name} />
+            <PublicationPreview title={this.props.data.title} picture={user.picture} name={user.name} />
+            <PublicationPreview title={this.props.data.title} picture={user.picture} name={user.name} />
           </div>
 
           <div className="four columns">
             <h2>Your Updates</h2>
-            You dont have any papers yet.  Create one..
+            You dont have any publications yet.  Create one..
           </div>
         </div>
       </div>
