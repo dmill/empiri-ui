f786e3251886f853f9831ebe9c147b72c219df8d
Andrew Wong <arwong09@gmail.com>
2015-12-24 20:07:10 -0800

    create avatar row

--- app/js/views/hypothesis_view.jsx
+++ app/js/views/hypothesis_view.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import store from '../redux/store'
+import AvatarRowComponent from '../components/avatar_row_component'
 import AvatarComponent from '../components/avatar_component'
 
 const Avatar = (props) => {
@@ -14,18 +15,28 @@ const Avatar = (props) => {
 export default class HypothesisView extends Component {
   render() {
     const data = {
-      title: "TIPR: transcription initiation pattern recognition on a genome scale"
+      title: "TIPR: transcription initiation pattern recognition on a genome scale",
+      abstract: "The computational identification of gene transcription start sites (TSSs) can provide insights into the regulation and function of genes without performing expensive experiments, particularly in organisms with incomplete annotations. High-resolution general-purpose TSS prediction remains a challenging problem, with little recent progress on the identification and differentiation of TSSs which are arranged in different spatial patterns along the chromosome."
     }
     const user = {
       user_id: "linkedin|2qdTsGouxA",
       picture: "https://media.licdn.com/mpr/mprx/0_Vu55r8ZE3voudRCP4WNnriodTqxuwZiP4wlVriEVYAwgNpq1nEvWY_Oq7s0tHxGxR7bU0kNQ7Yd9",
       name: "Andrew Wong"
     }
+    const avatars = [
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />,
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />,
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />,
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />,
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />,
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />,
+      <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />
+    ]
     return (
       <div id="hypothesis-view">
         <div className="row">
           <h1>{data.title}</h1>
-          <AvatarComponent id={user.user_id} name={user.name} imgSrc={user.picture} />
+          <AvatarRowComponent avatars={avatars} />
         </div>
         <div className="row">
           <h3>Abstract: {data.abstract}</h3>
