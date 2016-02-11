0b2b4e4bf7fd7a50d617d3b19ef5561054e5700a
Andrew Wong <arwong09@gmail.com>
2016-02-11 02:17:47 -0300

    fixes to App, takes in passed in children

--- app/js/application.jsx
+++ app/js/application.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 import auth0 from './auth0/auth0'
 import store from './redux/store'
 import NavBarView from './views/nav_bar_view'
-import startRouter from './router'
+import { startRouter } from './router'
 
-const App = () => {
+const App = ({ children, history, route }) => {
   return (
     <div id="layout">
-      <NavBarView lock={this.props.route.lock} history={this.props.history} />
-      {this.props.children}
+      <NavBarView lock={route.lock} history={history} />
+      {children}
     </div>
   )
 }
