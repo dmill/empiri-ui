85630f99a01c5b30f1aef29c1d44f8e96049543a
Andrew Wong <arwong09@gmail.com>
2016-02-19 19:47:26 -0300

    add stuff to publication view

--- app/js/views/publication_view.jsx
+++ app/js/views/publication_view.jsx
@@ -20,23 +20,27 @@ export default class PublicationView extends Component {
     this.state = {}
     const publicationId = this.props.routeParams.publicationId
     $.get(`http://localhost:4000/publications/${publicationId}`)
-      .done(({ publication }) => {
-        this.state = publication
-      })
+      .done(({ publication }) => this.setState(publication))
   }
 
   render() {
-    const user = {
-      user_id: "linkedin|2qdTsGouxA",
-      picture: "http://a.deviantart.net/avatars/y/_/y-u-no-guyplz.png?1",
-      name: "Andrew Wong"
+    if(!this.state.title) {
+      return <div></div>
     }
+    const user = this.state._embedded.users[0]
 
     return (
       <div id="publication-view" className="container">
         <div className="row">
-          <h1>Contributions</h1>
-          <Link to="/review-papers/new"><IconElement iconType="material" iconName="note_add" /></Link>
+          <h1>{this.state.title}</h1>
+          <img className="circle" src={user.photo_url} />
+          <div className="author-data">
+            <div className="author">{user.first_name} {user.last_name}</div>
+            <div className="updated-at">updated Jan 28, 2016</div>
+          </div>
+          <h2>Abstract</h2>
+          <p>{this.state.abstract}</p>
+          <Link to="/review-papers/new"><IconElement iconType="material" iconName="rate_review" /></Link>
         </div>
       </div>
     )
