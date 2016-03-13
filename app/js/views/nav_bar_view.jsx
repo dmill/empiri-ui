988ee312e85673973b8f8d3a3931cad4af98342b
Andrew Wong <arwong09@gmail.com>
2016-03-13 20:11:10 -0300

    fix logo path

--- app/js/views/nav_bar_view.jsx
+++ app/js/views/nav_bar_view.jsx
@@ -88,7 +88,7 @@ export default class NavBarView extends Component {
           <div className="container">
             <div className="row">
               <div className="five columns">
-                <Link className="link" to="/"><img id="logo" src="images/symbol.png" width="20px" /></Link>
+                <Link className="link" to="/"><img id="logo" src="/build/images/symbol.png" width="20px" /></Link>
                 <Link className="link" to="/browse"><h6>Browse</h6></Link>
                 <Link className="link" to="/faq"><h6>FAQ</h6></Link>
               </div>
